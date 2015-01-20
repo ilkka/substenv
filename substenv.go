6c0ce5f665aca146df5c732e9cf7de713eb1528d
Ilkka Laukkanen <ilkka@fastmail.fm>
2015-01-20 10:21:02 +0200

    Implement actual environment expansion

--- substenv.go
+++ substenv.go
@@ -16,6 +16,27 @@ var (
     re = regexp.MustCompile(`\$(?:\{([A-Z][A-Z0-9_]*)\}|([A-Z][A-Z0-9_]*)\b)`)
 )
 
+func template(line string) string {
+    var output = ""
+    var nextIncludedIndex = 0
+    matches := re.FindAllStringSubmatchIndex(line, -1)
+    for i := 0; i < len(matches); i++ {
+        match := matches[i]
+        // find indices of range to replace in line plus the range that
+        // gives the name of the variable
+        replaceRange := match[0:2]
+        var nameRange = match[2:4]
+        if nameRange[0] == -1 {
+            nameRange = match[4:6]
+        }
+        name := line[nameRange[0]:nameRange[1]]
+        output += line[nextIncludedIndex:replaceRange[0]] + os.Getenv(name)
+        nextIncludedIndex = replaceRange[1]
+    }
+    output += line[nextIncludedIndex:len(line)]
+    return output
+}
+
 func main() {
     kingpin.Version("0.0.1")
     kingpin.Parse()
@@ -29,10 +50,6 @@ func main() {
             }
             break
         }
-        matches := re.FindAllStringSubmatchIndex(line, -1)
-        fmt.Printf("%s -> %v\n", line, matches)
-        for i := 0; i < len(matches); i++ {
-            fmt.Printf("-- %v\n", matches[i])
-        }
+        fmt.Printf("%s", template(line))
     }
 }
