76bf69021a8de1b7d7f9018a297f5bec7162293f
Ilkka Laukkanen <ilkka@fastmail.fm>
2015-01-20 09:57:06 +0200

    Regex for detecting envsubst constructs
    Understands either $ASDF or ${ASDF}, the former of which has to be
    followed by a word boundary.

--- substenv.go
+++ substenv.go
@@ -6,12 +6,14 @@ import (
     "io"
     "log"
     "os"
+    "regexp"
     "gopkg.in/alecthomas/kingpin.v1"
 )
 
 var (
     app = kingpin.New("substenv", "Substitute environment variables into templates")
-    input = kingpin.Arg("input", "Input file or stdin if not given").File()
+    input = app.Arg("input", "Input file or stdin if not given").File()
+    re = regexp.MustCompile(`\$(?:\{([A-Z][A-Z0-9_]*)\}|([A-Z][A-Z0-9_]*)\b)`)
 )
 
 func main() {
@@ -20,13 +22,17 @@ func main() {
 
     var bio = bufio.NewReader(os.Stdin)
     for {
-        var line, _, err = bio.ReadLine()
+        var line, err = bio.ReadString('\n')
         if err != nil {
             if err != io.EOF {
                 log.Fatal(err)
             }
             break
         }
-        fmt.Printf("%s\n", line)
+        matches := re.FindAllStringSubmatchIndex(line, -1)
+        fmt.Printf("%s -> %v\n", line, matches)
+        for i := 0; i < len(matches); i++ {
+            fmt.Printf("-- %v\n", matches[i])
+        }
     }
 }
